a91705ca3d584aa0a5e2ea5af1f71dc7c54d0e98
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2022-01-03 16:30:51 -0600

    added depth_converter

--- codes/depth_conversion.py
+++ codes/depth_conversion.py
@@ -42,7 +42,7 @@ def per_video_depth_converter(depth_file: np.ndarray,
 
         # Filters current frame's depth information and resizes it.
         frame_depth = depth_file[:, :, i]
-        frame_depth_resized = cv2.resize(frame_depth, dsize=(640, 480))
+        frame_depth_resized = cv2.resize(frame_depth, dsize=(480, 640))
 
         # Filter current frame's skeleton point information
         current_frame_skeleton_points = skeleton_point_information[skeleton_point_information['frame'] == i]
@@ -67,17 +67,50 @@ def depth_converter(n_actions: int,
                     n_subjects: int,
                     n_takes: int,
                     skeleton_pose_models: list):
-    depth_file = loadmat('../data/original_data/Depth/a1_s1_t1_depth.mat')
-    data_name = 'a1_s1_t1'
-    skeleton_point_data = pd.read_csv('../data/processed_data/rgb/{}_{}.csv'.format(data_name, skeleton_pose_models[0]))
-    per_video_depth_converter(depth_file['d_depth'], 'coco', 'processed_data', 'depth', 'a1_s1_t1', skeleton_point_data)
+    """Converts Matlab depth information and adds them to the skeleton point information for all actions, subjects, and
+    takes.
 
+        Args:
+            n_actions: Total number of actions in the original dataset.
+            n_subjects: Total number of subjects in the original dataset.
+            n_takes: Total number of takes in the original dataset.
+            skeleton_pose_models: Model names which will be used to import model details.
+
+        Returns:
+            None.
+
+        Raises:
+            FileNotFoundError: If a particular video file is not found.
+    """
+    modality = 'depth'
+    data_version = 'processed_data'
+
+    # Iterates across all actions, subjects and takes in the dataset.
+    for i in range(1, n_actions + 1):
+        for j in range(1, n_subjects + 1):
+            for k in range(1, n_takes + 1):
+                for m in range(len(skeleton_pose_models)):
+                    data_name = 'a{}_s{}_t{}'.format(i, j, k)
+
+                    # Imports MATLAB depth file and the skeleton point information for the current action, subject and
+                    # take.
+                    try:
+                        depth_file = loadmat('../data/original_data/{}/{}_{}.mat'.format(modality.title(), data_name,
+                                                                                         modality))
+                        skeleton_point_information = pd.read_csv('../data/{}/rgb/{}_{}.csv'.format(data_version,
+                                                                                                   data_name,
+                                                                                                   skeleton_pose_models[m]))
+                        per_video_depth_converter(depth_file['d_depth'], skeleton_pose_models[m], data_version,
+                                                  modality, data_name, skeleton_point_information)
+                    except FileNotFoundError:
+                        print('Video file for {} does not exists'.format(data_name))
+                    print()
 
 
 def main():
     print()
-    n_actions = 5
-    n_subjects = 5
+    n_actions = 27
+    n_subjects = 8
     n_takes = 4
     skeleton_pose_models = ['coco', 'mpi']
     depth_converter(n_actions, n_subjects, n_takes, skeleton_pose_models)
