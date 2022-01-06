b20649300e9e62e099c82a9e47b3a9b9773b53ab
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2022-01-05 23:11:39 -0600

    updated data_normalizer

--- codes/data_normalization.py
+++ codes/data_normalization.py
@@ -23,7 +23,7 @@ def min_max_normalizer(processed_sensor_values: list):
     # If all the values in the processed sensor values is 0 then the normalization will not be done else normalization
     # will be performed.
     if maximum_value - minimum_value == 0:
-        normalized_sensor_values = processed_sensor_values
+        normalized_sensor_values = [0 for _ in range(len(processed_sensor_values))]
     else:
         normalized_sensor_values = [(processed_sensor_values[i] - minimum_value) / (maximum_value - minimum_value)
                                     for i in range(len(processed_sensor_values))]
@@ -68,12 +68,44 @@ def per_video_data_normalizer(skeleton_point_information: pd.DataFrame,
 def data_normalizer(n_actions: int,
                     n_subjects: int,
                     n_takes: int,
-                    skeleton_pose_models: list):
-    data_name = 'a1_s1_t1'
-    data_version = 0
+                    skeleton_pose_models: list,
+                    modalities: list):
+    """Performs min-max normalization on the skeleton point information for all actions, subjects, and takes.
+
+        Args:
+            n_actions: Total number of actions in the original dataset.
+            n_subjects: Total number of subjects in the original dataset.
+            n_takes: Total number of takes in the original dataset.
+            skeleton_pose_models: Model names which will be used to import model details.
+            modalities: List of modalities available in the dataset.
+
+        Returns:
+            None.
+    """
+    data_version = 'normalized_data'
+
+    # Iterates across all actions, subjects, takes, skeleton pose models and modalities in the dataset.
+    for i in range(1, n_actions + 1):
+        for j in range(1, n_subjects + 1):
+            for k in range(1, n_takes + 1):
+                for m in range(len(skeleton_pose_models)):
+                    for n in range(len(modalities)):
+                        data_name = 'a{}_s{}_t{}'.format(i, j, k)
+
+                        # Imports skeleton point information file and performs min-max normalization.
+                        try:
+                            skeleton_point_information = pd.read_csv(
+                                '../data/processed_data/{}/{}_{}.csv'.format(modalities[n], data_name,
+                                                                             skeleton_pose_models[m]))
+                            per_video_data_normalizer(skeleton_point_information, skeleton_pose_models[m], data_version,
+                                                      modalities[n], data_name)
+                        except FileNotFoundError:
+                            print('Video file for {}_{} does not exist.'.format(data_name, skeleton_pose_models[m]))
+                        print()
 
 
 def main():
+    print()
     n_actions = 27
     n_subjects = 8
     n_takes = 4
